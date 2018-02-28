41e3214295de0afc376dc1657c758e44bdc2df17
pabloaredu <pablo.adu83@gmail.com>
2018-02-28 04:01:42 +0000

    Added validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
 	belongs_to :store
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :store_id, presence: true
+	validates :hourly_rate, presence: true,
+			  numericality: { only_integer: true },
+			  inclusion: {in: 40..200}
 end
